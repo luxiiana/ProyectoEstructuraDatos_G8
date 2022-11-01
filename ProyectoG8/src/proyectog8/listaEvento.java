60c3051289291755db0abdebe878f6d58698913a
luxiiana <lu.azoque@gmail.com>
2022-10-31 19:28:52 -0600

    modulo 2 (no terminado)

--- ProyectoG8/src/proyectog8/listaEvento.java
+++ ProyectoG8/src/proyectog8/listaEvento.java
@@ -20,36 +20,64 @@ public class listaEvento {
         }
     }
     
-    public void agregarEvento(String nombreEvento, int fecha, String lugar){
-        //se crea objeto para nuestros datos
-        Evento e = new Evento();
+    public int agregarEvento(int numEventos){
         
-        //llenamos con los datos al objeto
-        e.setNombreEvento(JOptionPane.showInputDialog("Ingrese el nombre del evento: "));
-        e.setFecha(JOptionPane.showInputDialog("Ingrese la fecha del evento de la siguiente forma: \nEjemplo: 1 de enero, 2000\n"));
-        e.setLugar(JOptionPane.showInputDialog("Ingrese el lugar del evento: "));
+        numEventos = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la cantidad de eventos que agregará: "));
         
-        //se crea el objeto del nodo 
-        nodoEvento ne = new nodoEvento();
-        //se establece que nuestro nodo contiene los datos del primer objeto que creamos
-        ne.setDato(e);
-        
-        if(inicio.getDato().getLugar().equals("") && inicio.getDato().getFecha().equals("") && esVacio()){
+        if(numEventos==0){
+            return 0;
+        }else{
+            //se crea objeto para nuestros datos
+            Evento e = new Evento();
+
+            //llenamos con los datos al objeto
+            e.setNombreEvento(JOptionPane.showInputDialog("Ingrese el nombre del evento: "));
+            e.setFecha(JOptionPane.showInputDialog("Ingrese la fecha del evento de la siguiente forma: \nEjemplo: 1 de enero, 2000\n"));
+            e.setLugar(JOptionPane.showInputDialog("Ingrese el lugar del evento: "));
+
+            //se crea el objeto del nodo 
+            nodoEvento ne = new nodoEvento();
+            //se establece que nuestro nodo contiene los datos del primer objeto que creamos
+            ne.setDato(e);
             
-            //si el nodo de inicio es vacio (no tiene lugar ni fecha) entonces podemos agregar el evento
+            if(esVacio()){ //agregar si está vacío
+
+                //si el nodo de inicio es vacio (no tiene lugar ni fecha) entonces podemos agregar el evento
+
+                //agregar al inicio
+                inicio=ne;
+                fin=ne;
+
+                //referencia circular 
+                fin.setSiguiente(inicio);
+
+            }else if(e.getFecha().compareTo(inicio.getDato().getFecha()) <0){ //agregar a la izquierda
+
+                //si el dato de fecha es mayor a la fecha del inicio, el inicio pasa a siguiente y el inicio será el nuevo nodo
+                inicio.setSiguiente(inicio);
+                inicio=ne;
+
+                //referenica circular
+                fin.setSiguiente(inicio);
+
+            }else if(inicio.getDato().getFecha().compareTo(e.getFecha()) <0){ //agregar a la derecha
+                
+                //el dato que le sigue a inicio será el nodo nuevo
+                inicio.setSiguiente(ne);
+                
+                //referencia circular
+                fin.setSiguiente(inicio);
+                
+            }else{ //agrega en medio
+                
+            }
             
-            //agregar al inicio
-            inicio=ne;
-            fin=ne;
-            
-            //referencia circular 
-            fin.setSiguiente(inicio);
-            
-        }else if(!inicio.getDato().getLugar().equals("") && !inicio.getDato().getFecha().equals("")){
-            
-            
-            
+            //recursividad, la cantidad de eventos que dijimos al inicio va a disminuir hasta llegar al caso base
+            numEventos--;
+            return (agregarEvento(numEventos));
         }
+        
+        
         
     }
     
